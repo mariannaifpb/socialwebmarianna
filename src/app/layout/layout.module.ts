6de0195ef9174b24fe75979e89da2e3ca77685f7
mariannaifpb <112527988+mariannaifpb@users.noreply.github.com>
2022-10-03 20:38:16 -0300

    1 file modified

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button/button-module';
+import { MatMenuModule } from '@angular/material/menu/menu-module';
 
 @NgModule({
   imports: [
